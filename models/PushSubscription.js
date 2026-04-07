fe2d585f6fffe0e429f4f165591528e3ce3d61c7
Michael <9227552@gmail.com>
2026-04-07 20:52:50 +0300

    Remove PushSubscription model

--- models/PushSubscription.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const pushSubscriptionSchema = new mongoose.Schema({
-  subscription: { type: Object, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('PushSubscription', pushSubscriptionSchema);
